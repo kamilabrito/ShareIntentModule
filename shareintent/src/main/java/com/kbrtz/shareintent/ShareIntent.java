02e968147c94f6e6ea4b08786deecb6663d28ed3
kamilabrito <kamilabrito@Kamilas-MacBook-Pro.local>
2015-06-01 11:44:14 -0400

    Adding Share Intent Module files

---
+++ shareintent/src/main/java/com/kbrtz/shareintent/ShareIntent.java
@@ -0,0 +1,105 @@
+/**
+ This file is part of ShareIntent.
+
+ ShareIntent is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ Foobar is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.kbrtz.shareintent;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Environment;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ * Created by kamilabrito (kamilabrito.com) on 6/1/15.
+ */
+public class ShareIntent {
+
+    private ImageView image;
+    private Context context;
+
+    public ShareIntent (final ImageView image, final Context context) {
+        this.image = image;
+        this.context = context;
+    }
+
+    /**
+     * Share a image, jpg or png, thought Intent Share in Android
+     * @return return the intent thar will be shared
+     */
+    public Intent shareImage() {
+
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.setType("image/*");
+
+        Uri uri = getLocalBitmapUri(image);
+        shareIntent.putExtra(Intent.EXTRA_STREAM, uri);
+
+        return shareIntent;
+    }
+
+    /**
+     * Share text thought Intent Share in Android
+     * @param shareText Text you want to share
+     * @param subtitleText Text you want to be shown in the subject of the content you're sharing
+     * @return return the intent thar will be shared
+     */
+
+    public Intent shareText(String shareText, String subtitleText) {
+        Intent sendIntent = new Intent();
+        sendIntent.setAction(Intent.ACTION_SEND);
+        sendIntent.putExtra(Intent.EXTRA_SUBJECT, subtitleText);
+        sendIntent.putExtra(Intent.EXTRA_TEXT, shareText);
+        sendIntent.setType("text/plain");
+        sendIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+        return sendIntent;
+    }
+
+    // Returns the URI path to the Bitmap displayed in specified ImageView
+    private Uri getLocalBitmapUri(ImageView imageView) {
+        // Extract Bitmap from ImageView drawable
+        Drawable drawable = imageView.getDrawable();
+        Bitmap bmp = null;
+        if (drawable instanceof BitmapDrawable) {
+            bmp = ((BitmapDrawable) imageView.getDrawable()).getBitmap();
+        } else {
+            return null;
+        }
+        // Store image to default external storage directory
+        Uri bmpUri = null;
+        try {
+            File file = new File(Environment.getExternalStoragePublicDirectory(
+                    Environment.DIRECTORY_DOWNLOADS), "share_image_" + System.currentTimeMillis() + ".png");
+            file.getParentFile().mkdirs();
+            FileOutputStream out = new FileOutputStream(file);
+            bmp.compress(Bitmap.CompressFormat.PNG, 90, out);
+            out.close();
+            bmpUri = Uri.fromFile(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return bmpUri;
+    }
+
+}
